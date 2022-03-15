3a18bebc851dc202a764cc6f26159afa87e8c376
Adrian <adriandinca2008@gmail.com>
2022-03-16 00:37:07 +0200

    inline error message inside the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,16 @@
 import Layout from '@components/layout/layout';
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import ErrorInformation from '@components/404/error-information';
 import Seo from '@components/seo/seo';
+import Image from 'next/image';
+import { useTranslation } from 'next-i18next';
+
+const backgroundThumbnail = '/assets/images/404-bg.png';
+const errorThumbnail = '/assets/images/404.png';
 
 export default function ErrorPage() {
+  const { t } = useTranslation('common');
+
   return (
     <>
       <Seo
@@ -12,7 +18,29 @@ export default function ErrorPage() {
         description="Get your fresh food delivered to your doorstep."
         path="404"
       />
-      <ErrorInformation />
+
+      <div
+        className="text-center px-12 py-16 sm:py-20 lg:py-24 xl:py-32 flex items-center justify-center bg-cover bg-no-repeat bg-center"
+        style={{
+          backgroundImage: `url(${backgroundThumbnail})`,
+        }}
+      >
+        <div className="max-w-md xl:max-w-lg">
+          <Image
+            src={errorThumbnail}
+            alt={t('error-heading')}
+            width={150}
+            height={150}
+          />
+
+          <h2 className="text-6xl md:text-7xl 2xl:text-8xl font-bold text-skin-base pt-5 xl:pt-9">
+            {t('error-heading')}
+          </h2>
+          <p className="text-15px md:text-base 2xl:text-[18px] leading-7 md:leading-8 pt-4 font-medium">
+            {t('error-sub-heading')}
+          </p>
+        </div>
+      </div>
     </>
   );
 }
